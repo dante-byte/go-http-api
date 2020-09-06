a9a58a18fd6b412368ab8a294944be09ede17c0d
Dante White <559.dantewhite@gmail.com>
2020-09-06 07:55:41 -0400

    api

--- main/Hey.go
+++ main/Hey.go
@@ -2,10 +2,24 @@ package main
 
 import (
 	"fmt"
+	"github.com/gorilla/mux"
+	//"encoding/json"
+	"net/http"
 )
+
+func test(w http.ResponseWriter, r *http.Request)  {
+
+	w.Write([]byte("this is a test"))
+
+}
 
 func main() {
 
-	fmt.Print("hello")
+	fmt.Print("hel")
+	router := mux.NewRouter()
+
+	router.HandleFunc("/test",test)
+
+	http.ListenAndServe(":5000",router)
 	
 }
