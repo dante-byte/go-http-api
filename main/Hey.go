48120b3f3a0452a6095973b060e1239f44ccc2af
Dante White <559.dantewhite@gmail.com>
2020-09-11 06:49:55 -0400

    goapi

--- main/Hey.go
+++ main/Hey.go
@@ -9,43 +9,36 @@ import (
 
 
 
+type Item struct {
+
+	data string
+}
 
 
-var data []string = []string{} // composite literal string slice
+var data []Item = []Item{} // composite literal string slice
 
 func main() {
 
 
 	router := mux.NewRouter()
 
-	router.HandleFunc("/test",test)
-
-
-	router.HandleFunc("/add/{item}", addItem)
-
+	router.HandleFunc("/add", addItem).Methods("POST") //{} dynamive varial identifer value .METHODS ALLOWS CERTER HTTP REUEST
 	http.ListenAndServe(":5000",router)
 	
 }
 
 // route handlers
 
-func test(w http.ResponseWriter, r *http.Request)  {
 
+func addItem(w http.ResponseWriter, r *http.Request)  { // * pointer type https.reqyest
 	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(data) // encode the data from teh slice
+
+	var newItem Item
+	json.NewDecoder(r.Body).Decode(&newItem)
 
 
-
-	json.NewEncoder(w).Encode(struct { // anonymous struct, a predefined type isn't needed
-		ID string //fields
-	}{ID: "5"}) // value set
-}
-
-func addItem(w http.ResponseWriter, r *http.Request)  {
-
-	routeVar := mux.Vars(r)["item"] // gives access to data
-	data = append(data, routeVar)
-
-	json.NewEncoder(w).Encode(data)
+	data = append(data, newItem)
 
 
 }
